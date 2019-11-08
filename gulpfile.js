6b46b0061f5f9a082455f83320983546465a2c49
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2019-11-08 11:50:36 -0300

    Seção 3 - Iniciando a construção da API - Aula 17 - Com erro

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,40 @@ const clean = require("gulp-clean");
 
 const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("scripts", ["static"], () => {
+// CÓDIGO PARA VERSÃO 4 DO GULP
+
+scripts = () => {
 	const tsResult = tsProject.src().pipe(tsProject());
 	return tsResult.js.pipe(gulp.dest("dist"));
-});
+};
 
-gulp.task("static", ["clean"], () => {
+static = () => {
 	return gulp.src(["src/**/*.json"]).pipe(gulp.dest("dist"));
-});
+};
 
-gulp.task("clean", () => {
-	return gulp.src("dist").pipe(clean());
-});
+cleans = () => {
+	gulp.src("dist").pipe(clean());
+};
 
-gulp.task("build", ["clean", "static", "scripts"]);
+watchs = () => {
+	gulp.watch(["src/**/*.ts", "src/**/*.json"]);
+};
+
+exports.default = gulp.series(static, scripts, watchs);
+
+// CÓDIGO PARA VERSÃO 3 DO GULP
+
+// gulp.task("scripts", () => {
+// 	const tsResult = tsProject.src().pipe(tsProject());
+// 	return tsResult.js.pipe(gulp.dest("dist"));
+// });
+
+// gulp.task("static", () => {
+// 	return gulp.src(["src/**/*.json"]).pipe(gulp.dest("dist"));
+// });
+
+// gulp.task("clean", () => {
+// 	return gulp.src("dist").pipe(clean());
+// });
+
+// gulp.task("build", ["clean", "static", "scripts"]);
