1a2402af1a81216668fcde74dc1d6d0bb3bf4a3f
Paulo Roberto <prmorais1302@gmail.com>
2019-11-28 17:18:40 -0300

    Ajustes no código

--- src/graphql/resources/user/user.resolvers.ts
+++ src/graphql/resources/user/user.resolvers.ts
@@ -4,51 +4,51 @@ import { Transaction } from "sequelize";
 import { UserAttributes } from "../../../models/UserModel";
 
 export const userResolvers = {
-  Query: {
-    users: (
-      _parent: any,
-      { first = 10, offset = 0 },
-      { db }: { db: DbConnection },
-      info: GraphQLResolveInfo
-    ) => {
-      return db.User.findAll({ limit: first, offset: offset });
-    },
+	Query: {
+		users: (
+			_parent: any,
+			{ first = 10, offset = 0 },
+			{ db }: { db: DbConnection },
+			info: GraphQLResolveInfo
+		) => {
+			return db.User.findAll({ limit: first, offset: offset });
+		},
 
-    user: async (
-      _parent: any,
-      { id }: any,
-      { db }: { db: DbConnection },
-      info: GraphQLResolveInfo
-    ) => {
-      const user = await db.User.findOne({ where: id });
-      if (!user) throw new Error(`Usuário com ID ${id} não encontrado!`);
-      return user;
-    }
-  },
+		user: async (
+			_parent: any,
+			{ id }: any,
+			{ db }: { db: DbConnection },
+			info: GraphQLResolveInfo
+		) => {
+			const user = await db.User.findByPk(id);
+			if (!user) throw new Error(`Usuário com ID ${id} não encontrado!`);
+			return user;
+		}
+	},
 
-  Mutation: {
-    createUser: (
-      _parent: any,
-      { input }: { input: UserAttributes },
-      { db }: { db: DbConnection },
-      info: GraphQLResolveInfo
-    ) => {
-      return db.sequelize.transaction(async (t: Transaction) => {
-        return await db.User.create(input, { transaction: t });
-      });
-    },
+	Mutation: {
+		createUser: (
+			_parent: any,
+			{ input }: { input: any },
+			{ db }: { db: DbConnection },
+			info: GraphQLResolveInfo
+		) => {
+			return db.sequelize.transaction(async (t: Transaction) => {
+				return await db.User.create(input, { transaction: t });
+			});
+		},
 
-    updateUser: (
-      _parent: any,
-      { id, input }: any,
-      { db }: { db: DbConnection },
-      info: GraphQLResolveInfo
-    ) => {
-      return db.sequelize.transaction(async (t: Transaction) => {
-        const user = await db.User.findOne(id);
-        if (!user) throw new Error(`Usuário com ID ${id} não encontrado!`);
-        return db.User.update(input, { transaction: t, where: id });
-      });
-    }
-  }
+		updateUser: (
+			_parent: any,
+			{ id, input }: any,
+			{ db }: { db: DbConnection },
+			info: GraphQLResolveInfo
+		) => {
+			return db.sequelize.transaction(async (t: Transaction) => {
+				const user = await db.User.findOne(id);
+				if (!user) throw new Error(`Usuário com ID ${id} não encontrado!`);
+				return db.User.update(input, { transaction: t, where: id });
+			});
+		}
+	}
 };
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,25 +1,25 @@
 import {
-  Sequelize,
-  Model,
-  DataTypes,
-  CreateOptions,
-  BuildOptions
+	Sequelize,
+	Model,
+	DataTypes,
+	CreateOptions,
+	BuildOptions
 } from "sequelize";
 import { genSaltSync, hashSync, compareSync } from "bcryptjs";
 import { BaseModelInterface } from "../interfaces/BaseModelInterface";
 
 export interface UserAttributes {
-  id?: number;
-  name?: string;
-  email?: string;
-  password?: string;
-  photo?: string;
-  createdAt?: string;
-  updatedAt?: string;
+	id?: number;
+	name?: string;
+	email?: string;
+	password?: string;
+	photo?: string;
+	createdAt?: string;
+	updatedAt?: string;
 }
 
 export interface UserInstance extends UserAttributes {
-  isPassword(encondedPassword: string, password: string): boolean;
+	isPassword(encondedPassword: string, password: string): boolean;
 }
 
 // export interface UserModel
@@ -27,49 +27,49 @@ export interface UserInstance extends UserAttributes {
 // 		Model<UserInstance, UserAttributes> {}
 
 export type UserModel = typeof Model & {
-  new (values?: object, options?: BuildOptions): any;
+	new (values?: object, options?: BuildOptions): UserAttributes;
 };
 
 export default (sequelize: Sequelize): UserModel => {
-  const User = <UserModel>sequelize.define(
-    "User",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      name: { type: DataTypes.STRING(128), allowNull: false },
-      email: { type: DataTypes.STRING(128), allowNull: false, unique: true },
-      password: {
-        type: DataTypes.STRING(128),
-        allowNull: false,
-        validate: { notEmpty: true }
-      },
-      photo: {
-        type: DataTypes.BLOB({ length: "long" }),
-        allowNull: true,
-        defaultValue: null
-      }
-    },
-    {
-      tableName: "users",
-      hooks: {
-        beforeCreate: (user: any, options: CreateOptions): void => {
-          const salt = genSaltSync();
-          user.password = hashSync(user.password, salt);
-        }
-      }
-    }
-  );
+	const User = <UserModel>sequelize.define(
+		"User",
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			name: { type: DataTypes.STRING(128), allowNull: false },
+			email: { type: DataTypes.STRING(128), allowNull: false, unique: true },
+			password: {
+				type: DataTypes.STRING(128),
+				allowNull: false,
+				validate: { notEmpty: true }
+			},
+			photo: {
+				type: DataTypes.BLOB({ length: "long" }),
+				allowNull: true,
+				defaultValue: null
+			}
+		},
+		{
+			tableName: "users",
+			hooks: {
+				beforeCreate: (user: any, options: CreateOptions): void => {
+					const salt = genSaltSync();
+					user.password = hashSync(user.password, salt);
+				}
+			}
+		}
+	);
 
-  User.prototype.isPassword = (
-    encondedPassword: string,
-    password: string
-  ): boolean => {
-    return compareSync(password, encondedPassword);
-  };
+	User.prototype.isPassword = (
+		encondedPassword: string,
+		password: string
+	): boolean => {
+		return compareSync(password, encondedPassword);
+	};
 
-  return User;
+	return User;
 };
